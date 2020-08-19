2f3e3f46280f136186a694cd7af61052d3fa41e8
1_KingEr <53094736+1kinger@users.noreply.github.com>
2020-08-19 06:00:45 +0100

    .

--- cristaylisBUY_files/JS/srkt.js
+++ cristaylisBUY_files/JS/srkt.js
@@ -117,22 +117,6 @@ for(i=0;i<NBR.length;i++){NBR[i].type="number";NBR[i].disabled=false;}}}
 
 
 
-function DLND(){
-	
-	var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var myArr = JSON.parse(this.responseText);
-    A = myArr;
-  }
-};
-xmlhttp.open("GET", "prjA.txt", true);
-xmlhttp.send();
-	
-	
-}
-
-
 
 
 
